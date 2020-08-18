7ac37d178e3f1c99477ef339ea93807b02389164
anshuman-patra <anshumanpatra845@gmail.com>
2020-08-18 21:55:35 +0530

    montly wage

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,35 +1,34 @@
 public class EmployeeWage
 {
+	public static final int FULL_TIME=1;
+	public static final int PART_TIME=2;
+	public static final int EMP_RATE_PER_HOUR=20;
+	public static final int NUMBER_WORKING_DAYS=20;
 	public static void main(String[] args)
 	{
 		System.out.println("Welcome to employee wage problem");
-
 		int empHrs=0;
-                int empWage=0;
-                int wagePerHour=20;
-		int randomCheck=(int) (Math.random()*10)%3;
-		switch(randomCheck)
-		     {
+		int empWage=0;
+		int totalSalary=0;
 
-		case 1:
-		          
-			System.out.println("Employee is present");
-			empHrs=8;
-			      break;
-				
-              case 2:
-		  System.out.println("Employee is part time");
-			empHrs=4;
-				break;
-		
-
-		 default:
-		
-			System.out.println("Employee is absent");
-				empHrs=0;
-				break;
+		for(int i=0; i<NUMBER_WORKING_DAYS; i++)
+		{
+			int randomCheck=(int) (Math.random()*10)%3;
+			switch(randomCheck)
+			{
+				case FULL_TIME:
+					empHrs=8;
+					break;
+				case PART_TIME:
+					empHrs=4;
+					break;
+				default:
+					empHrs=0;
 			}
-			empWage=wagePerHour*empHrs;
-		System.out.println("Employee daily wage:" + empWage);
+			empWage=EMP_RATE_PER_HOUR*empHrs;
+			System.out.println("Employee daily wage:" + empWage);
+			totalSalary=totalSalary+empWage;
+		}
+		System.out.println("Total employee wage:" + totalSalary);
 	}
 }
